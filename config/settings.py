b0c60d102bf7a34043385724e151169fe8109e78
Dmitry <mikshindima89@hmail.com>
2026-05-16 09:56:24 +0700

    Use Yandex SMTP

--- config/settings.py
+++ config/settings.py
@@ -160,9 +160,10 @@ FCM_SERVER_KEY = os.getenv('FCM_SERVER_KEY', '')
 
 # ── Email ─────────────────────────────────────────────────────  ← добавь сюда
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', 'noreply@auto-helper.ru')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', '')
