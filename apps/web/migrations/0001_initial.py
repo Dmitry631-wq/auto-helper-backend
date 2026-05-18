394e87f72fc9ee1597021e0b710c3464e4fdd489
Dmitry631-wq <mikshindima89@gmail.com>
2026-05-18 17:16:07 +0700

    Update 0001_initial.py

--- apps/web/migrations/0001_initial.py
+++ apps/web/migrations/0001_initial.py
@@ -1,10 +1,34 @@
-from django.db import migrations
+from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = []
+    dependencies = [
+        ('users', '0001_initial'),
+    ]
 
-    operations = []
+    operations = [
+        migrations.CreateModel(
+            name='AppRating',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True)),
+                ('stars', models.PositiveSmallIntegerField(verbose_name='Оценка')),
+                ('comment', models.TextField(blank=True, verbose_name='Комментарий')),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('user', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='app_ratings',
+                    to='users.user',
+                    verbose_name='Пользователь'
+                )),
+            ],
+            options={
+                'verbose_name': 'Оценка приложения',
+                'verbose_name_plural': 'Оценки приложения',
+                'unique_together': {('user',)},
+            },
+        ),
+    ]
